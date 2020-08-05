c53d0189a331e09622c6dc8485626c6972a6e482
rsurpur20 <42478922+rsurpur20@users.noreply.github.com>
2020-08-05 15:03:23 -0400

    Add files via upload

--- StoryBoardIntro/ViewController.swift
+++ StoryBoardIntro/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //makes the outlet for the label
+    @IBOutlet weak var appTitle: UILabel!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("this is a print statement - test test")
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if let newTitle = textField.text { //will work only if there is text in the text field
+            appTitle.text = newTitle
+//            var number1 = Int.random(in: 0 ... 256)
+//            var number2 = Int.random(in: 0 ... 256)
+//            var number3 = Int.random(in: 0 ... 256)
+//            appTitle.textColor = UIColor(red: CGFloat(number1), green: CGFloat(number2), blue: CGFloat(number3), alpha: 1.0)
+            
+        }
+    }
+    
 }
 
